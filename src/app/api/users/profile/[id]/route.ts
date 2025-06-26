9a802dc4dccfc8bee2f36704a42f467a5286e9e5
Sheikh Javeed <sheikhmannanjaveed@gmail.com>
2025-06-26 13:49:26 +0530

    edited route.ts

--- src/app/api/users/profile/[id]/route.ts
+++ src/app/api/users/profile/[id]/route.ts
@@ -4,21 +4,24 @@ import User from "@/models/userModel";
 
 connectToDB();
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(req: Request, context: { params: { id: string } }) {
     try {
-        console.log("Received request for user ID:", params.id);
-        
-        if (!params.id) {
+        const { id } = context.params;
+        console.log("Received request for user ID:", id);
+
+        if (!id) {
             return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
         }
-        const user = await User.findById(params.id).select("-password");
+
+        const user = await User.findById(id).select("-password");
 
         if (!user) {
             return NextResponse.json({ error: "User not found" }, { status: 404 });
         }
 
         return NextResponse.json({ user }, { status: 200 });
-    } catch (error: any) {
+    } catch (error) {
+        console.error("Error fetching user:", error);
         return NextResponse.json({ error: "Invalid user ID" }, { status: 400 });
     }
 }
